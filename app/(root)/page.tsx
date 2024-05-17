9ea831e32deea0c4593a31b09bde2f866ed97823
ricardoa23 <ricardo.a.ayala86@gmail.com>
2024-05-17 17:09:37 -0500

    creates a header component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,22 @@
+import HeaderBox from '@/components/HeaderBox'
 import React from 'react'
 
 const Home = () => {
+    const loggedIn = {firstName: "Rick"}
   return (
-    <div>Home</div>
+    <section className="home">
+        <div className="home-content">
+            <header className="home-header">
+                <HeaderBox 
+                type="greeting"
+                title="welcome"
+                user={loggedIn?.firstName || 'Guest'}
+                subtext="Access and manage your accounts and view Your transactions"
+                />
+                
+            </header>
+        </div>
+    </section>
   )
 }
 
