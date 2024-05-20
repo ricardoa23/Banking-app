3d88b4beac3833c7a8e80367d538ca30a1909103
ricardoa23 <ricardo.a.ayala86@gmail.com>
2024-05-20 18:39:44 -0500

    implements sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,17 +1,16 @@
 import Sidebar from "@/components/Sidebar";
 
-
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: "Rick", lastName: "Bobby" };
 
-  
   return (
-    <main className='flex h-screen w-full font-inter'>
-        <Sidebar />
-        {children}
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn} />
+      {children}
     </main>
   );
 }
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,9 +1,54 @@
-import React from 'react'
+'use client'
 
-const Sidebar = () => {
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { sidebarLinks } from "@/constants";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
+
+const Sidebar = ({ user }: SiderbarProps) => {
+  const pathName = usePathname();
+
   return (
-    <div>Sidebar</div>
-  )
-}
+    <section className="sidebar">
+      <nav className="flex flex-col gap-4">
+        <Link href="/" className="mb-12 cursor-pointer item-center gap-2 flex">
+          <Image
+            src="/icons/logo.svg"
+            width={34}
+            height={34}
+            alt="Logo"
+            className="size-[24px] max-xl:size-14"
+          />
+          <h1 className="sidebar-logo">Horizon</h1>
+        </Link>
 
-export default Sidebar
+        {sidebarLinks.map((item) => {
+          const isActive =
+            pathName === item.route || pathName.startsWith(`${item.route}/`);
+
+          return (
+            <Link
+              href={item.route}
+              key={item.label}
+              className={cn("sidebar-link", { "bg-bank-gradient": isActive})}
+            >
+              <div className='relative size-6'>
+                <Image
+                src={item.imgURL}
+                alt={item.label}
+                fill
+                className={cn({'brightness-[3] invert-0': isActive})}
+               />
+               </div>
+               <p className={cn('sidebar-label', {'!text-white': isActive})}>{item.label}</p>
+            </Link>
+          );
+        })}
+      </nav>
+    </section>
+  );
+};
+
+export default Sidebar;
