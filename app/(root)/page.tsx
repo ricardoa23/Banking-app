aa281a4d520973b65ca75d687069cb747ef747a9
ricardoa23 <ricardo.a.ayala86@gmail.com>
2024-05-21 18:45:01 -0500

    commiting to save progress

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,28 +1,32 @@
-import HeaderBox from '@/components/HeaderBox'
-import TotalBalanceBox from '@/components/TotalBalanceBox'
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import React from "react";
 
 const Home = () => {
-    const loggedIn = {firstName: "Rick"}
+  const loggedIn = { firstName: "Rick", lastName: "James", email: "RickJames@email.com" };
   return (
     <section className="home">
-        <div className="home-content">
-            <header className="home-header">
-                <HeaderBox 
-                type="greeting"
-                title="Welcome"
-                user={loggedIn?.firstName || 'Guest'}
-                subtext="Access and manage your accounts and view Your transactions"
-                />
-                <TotalBalanceBox
-                accounts={[]}
-                totalBanks={1}
-                totalCurrentBalance={1250.23}
-                 />
-            </header>
-        </div>
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your accounts and view Your transactions"
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.23}
+          />
+        </header>
+        {/* TODO: add recent transactions */}
+        PlaceHolder
+      </div>
+      <RightSidebar user={loggedIn} transactions={[]} banks={[]} />
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
